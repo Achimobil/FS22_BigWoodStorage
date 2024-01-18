29c6a04d0edeb9265683a170b99e2ac0bc4a1acf
Achim Kuntz <achimobil@hotmail.com>
2024-01-18 14:35:58 +0100

    ignore cmd

--- TestLocal-3.cmd
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_BigWoodStorage.zip" -x@FileDefinition.txt
-"C:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -autoStartSavegameId 3 -cheats -profile E:\LS22Profile\LS22ModTest -devWarning
